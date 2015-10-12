579ea9bf4101ef234ea6a572b814d7912c3360eb
xxxpokemon <c.marosvolgyi@gmail.com>
2015-10-12 19:53:48 +0200

    new file:   drmartens/chip.pdf 	modified:   firmware/firmware.ino 	modified:   rekenmachientje.c

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,7 +2,10 @@
 
 void setup() {
   // put your setup code here, to run once:
-  setPwmFrequency(5, 1);
+  //setPwmFrequency(5, 1);
+  //https://www.arduino.cc/en/Tutorial/SecretsOfArduinoPWM
+  //we moeten de pwm freq naar 60kHz doen
+  
 }
   int val;
   
--- rekenmachientje.c
+++ rekenmachientje.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int optellen(int term1, int term2)
+{
+	if ((term1<0) | (term2<0)){
+		printf("een van de termen is negatief, ik ga aftrekken\n");
+	}
+	return term1 + term2;
+}
+
+int aftrekken(int term1, int term2)
+{
+	return term1 - term2;
+}
+
+int delen(int term1, int term2)
+{
+	if (term2==0){
+		printf("delen door nul is flauwe kul\n");
+		return 0;
+	} 
+	return term1/term2; 
+}
+
 int main(int argc,char *argv[])
 {
 	int i=0;
@@ -18,19 +41,14 @@ int main(int argc,char *argv[])
 	operator=atoi(argv[3]);	
 	
 	if (operator==1)
-		printf("de som van %i en %i = %i\n	",term1,term2,term1+term2);
+
+		printf("de som van %i en %i = %i\n	",term1,term2,optellen(term1, term2));
 	if (operator==2)
 		printf("Het product van %i en %i = %i\n",term1,term2,term1*term2);
-
 	if (operator==3)
 		printf("Het verschil van %i en %i = %i\n",term1,term2,term1-term2);
-
 	if (operator==4){
-		if (term2==0){
-			printf("delen door nul is flauwe kul!\n");
-			return 0; 
-		}
-		printf("Het quotient van %i en %i = %i\n",term1,term2,term1/term2);
+		printf("Het quotient van %i en %i = %i\n",term1,term2,delen(term1, term2));
 	}
 
 	return 0;
