4e1273df30f3543430c554a8fa52ffc511d638d8
xxxpokemon <c.marosvolgyi@gmail.com>
2015-10-12 20:59:17 +0200

    new file:   artwork/amplifier_bb.png 	new file:   breadboard/amplifier.fzz 	modified:   firmware/firmware.ino

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,12 +5,19 @@ void setup() {
   //setPwmFrequency(5, 1);
   //https://www.arduino.cc/en/Tutorial/SecretsOfArduinoPWM
   //we moeten de pwm freq naar 60kHz doen
-  
+  //TCCR1A=0b10000011;
+  //TCCR1B=0b00001001;
+
+  TCCR1A = ((1 << COM1A1) | (1 << WGM11) | (1 << WGM10));
+  TCCR1B = ((1 << WGM12) | (1 << CS10));
+  DDRB = (1 << 1);
+    
 }
-  int val;
+  
+int val;
   
 void loop() {
   // put your main code here, to run repeatedly:
   val = analogRead(ANALOG0);
-  analogWrite(5, val);
+  OCR1A = val;
 }
